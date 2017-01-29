bf07c31a3f0a0f89b81ed295851dbc20c2e95366
Rares Vasile <raresvasile2016@gmail.com>
2017-01-29 23:33:48 +0200

    login updates

--- src/main/java/org/fasttrackit/automation/LoginPage.java
+++ src/main/java/org/fasttrackit/automation/LoginPage.java
@@ -3,6 +3,8 @@ package org.fasttrackit.automation;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class LoginPage {
 
@@ -12,11 +14,26 @@ public class LoginPage {
     private WebElement passField;
     @FindBy (className = "login-btn")
     private WebElement loginBtn;
+    @FindBy (xpath = "//form//div[contains(@class, 'error-msg')]")
+    private WebElement errorMsg;
 
 
     public void login(String email, String pass) {
         emailField.sendKeys(email);
         passField.sendKeys(pass);
         loginBtn.click();
+
     }
+
+    public void invalidLogin(String email, String pass, String errorDisplayed) {
+        emailField.sendKeys(email);
+        passField.sendKeys(pass);
+        loginBtn.click();
+
+        assertThat("Message is not visible", errorMsg.isDisplayed());
+        assertThat("The message is visible", errorMsg.getText(),is(errorDisplayed));
+
+    }
+
+
 }
