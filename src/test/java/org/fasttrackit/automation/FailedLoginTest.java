9db51521751b75e6e92758db9a14d665b3f7b36b
Rares Vasile <raresvasile2016@gmail.com>
2017-01-29 23:34:12 +0200

    login updates

---
+++ src/test/java/org/fasttrackit/automation/FailedLoginTest.java
@@ -0,0 +1,67 @@
+package org.fasttrackit.automation;
+
+import org.fasttrackit.util.TestBase;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class FailedLoginTest extends TestBase {
+
+    private LoginPage failedLoginPage;
+
+
+    public FailedLoginTest() {
+
+        failedLoginPage = PageFactory.initElements(driver, LoginPage.class);
+
+    }
+
+    @Test
+
+    public void invalidLoginWithoutPassword() {
+
+        openBrowser();
+
+        failedLoginPage.invalidLogin("eu@fast.com", "", "Please enter your password!");
+
+
+    }
+
+    @Test
+
+    public void invalidLoginWithoutEmail() {
+
+        openBrowser();
+
+        failedLoginPage.invalidLogin("", "eu.pass", "Please enter your email!");
+
+
+    }
+
+    @Test
+
+    public void invalidLoginWithNoEmailAndPassword() {
+
+        openBrowser();
+
+        failedLoginPage.invalidLogin("", "", "Please enter your email!");
+
+
+    }
+
+    @Test
+
+    public void invalidLoginWithWrongEmailAndPassword() {
+
+        openBrowser();
+
+        failedLoginPage.invalidLogin("ciupi@wrg.com", "eu.passt", "Invalid user or password!");
+
+
+    }
+
+}
