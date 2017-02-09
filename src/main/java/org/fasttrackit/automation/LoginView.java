c282ae344ee271625d5140baeca709a497ee9130
Rares Vasile <raresvasile2016@gmail.com>
2017-02-09 21:11:43 +0200

    assert added

--- src/main/java/org/fasttrackit/automation/LoginView.java
+++ src/main/java/org/fasttrackit/automation/LoginView.java
@@ -1,7 +1,10 @@
 package org.fasttrackit.automation;
 
 
+import com.sdl.selenium.extjs6.form.CheckBox;
+import com.sdl.selenium.web.SearchType;
 import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.utils.Utils;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -31,17 +34,27 @@ public class LoginView {
 //    private WebElement errorMsg;
 
     private WebLocator errorMsg = new WebLocator().setElPath("//form//div[contains(@class, 'error-msg')]");
-    public WebLocator stopTheProcess = new WebLocator().setText("\n" +
-            "            Stop the process?\n" +
-            "        ");
+    public WebLocator labelStopTheProcess = new WebLocator().setText("Stop the process?", SearchType.TRIM);
+    public WebLocator labelWithError = new WebLocator().setText("Label with Enter.", SearchType.TRIM, SearchType.CHILD_NODE);
+    public CheckBox checkBoxStopTheProcess = new CheckBox(labelStopTheProcess).setElPath("//input");
+    public CheckBox checkBoxLabelWithError = new CheckBox(labelWithError);
 
-    public WebLocator labelWithError = new WebLocator().setText("\n" +
-            "            Label with Enter.\n" +
-            "        ");
+
 
     public WebLocator techButton = new WebLocator("clearfix");
 
-    public WebLocator manualButton = new WebLocator().setText("Manual");
+    public WebLocator manualButton = new WebLocator().setElPath("//span[@class='pull-left'][normalize-space(text())='Manual']");
+    public WebLocator autoButton = new WebLocator().setElPath("//span[@class='pull-left'][normalize-space(text())='Auto']");
+
+    public static void main(String[] args) {
+        LoginView test = new LoginView();
+        System.out.println(test.labelStopTheProcess.getSelector());
+        System.out.println(test.labelWithError.getSelector());
+        System.out.println(test.checkBoxLabelWithError.getSelector());
+        System.out.println(test.checkBoxStopTheProcess.getSelector());
+    }
+
+
     public void login(String email, String pass) {
         emailField.sendKeys(email);
         passField.sendKeys(pass);
@@ -50,13 +63,22 @@ public class LoginView {
     }
 
     public void clickOnLabels(){
-        stopTheProcess.click();
+        labelStopTheProcess.click();
         labelWithError.click();
 
+        Utils.sleep(2000);
+
+        checkBoxStopTheProcess.click();
+        checkBoxLabelWithError.click();
+
+        assertThat("Stop the process is not selected!", checkBoxStopTheProcess.isSelected(), is(true));
+        assertThat("Label with Enter", checkBoxLabelWithError.isSelected(), is(true));
     }
 
     public void selectTech(){
         techButton.click();
+
+        Utils.sleep(1000);
 
         manualButton.click();
     }
