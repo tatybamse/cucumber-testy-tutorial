b90428c1e35b4f9242b49bf4bf03e1079482ee23
Rares Vasile <raresvasile2016@gmail.com>
2017-02-22 14:51:05 +0200

    checkbox from raw container

--- src/test/java/org/fasttrackit/automation/TablesTest.java
+++ src/test/java/org/fasttrackit/automation/TablesTest.java
@@ -2,6 +2,9 @@ package org.fasttrackit.automation;
 
 
 import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.table.Cell;
+import com.sdl.selenium.web.table.Row;
+import com.sdl.selenium.web.table.Table;
 import com.sdl.selenium.web.utils.Utils;
 import org.fasttrackit.util.TestBase;
 import org.junit.Test;
@@ -29,9 +32,24 @@ public class TablesTest extends TestBase {
         tableCheckBox = new WebLocator().setElPath("//table//tr[2]//input");
 
         tableCheckBox.click();
+    }
 
+    @Test
 
+    public void selectTableRowTest() {
 
+        openBrowser();
+
+        page.login("eu@fast.com", "eu.pass");
+
+        Table table =new Table();
+
+        Row row = table.getRow(new Cell(4, "davidmiller@mail.com" ));
+
+         WebLocator checkbox = new WebLocator(row).setTag("input");
+         checkbox.click();
 
     }
+
+
 }
